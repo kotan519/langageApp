84fb08e1dcff3b2127e6e2d8c7ae827be24a502f
KotaTashiro <kota.tashiro@stunited.vn>
2024-10-09 20:26:40 +1000

    fixed some errors

--- src/router/DeckRoutes.tsx
+++ src/router/DeckRoutes.tsx
@@ -1,5 +1,4 @@
 import { DeckList } from "../components/pages/deck/DeckList";
-import { VocaCards } from "../components/pages/deck/VocaCards";
 import { Home } from "../components/pages/home/Home";
 import { UserData } from "../components/pages/home/UserData";
 
